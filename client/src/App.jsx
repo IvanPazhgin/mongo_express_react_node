2dc6a0298c1e379a9a241b75239d570b0ed66a5e
IvanPazhgin <ivan.pazhgin@gmail.com>
2023-02-21 15:33:12 +0300

    работа над компонентом loader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,15 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import {useAuth} from "./hooks/auth.hook";
 import {AuthContext} from "./context/AuthContext";
 import {Navbar} from "./components/navbar";
+import {Loader} from "./components/loader";
 
 function App() {
-  const { login, logout, token, userId } = useAuth()
+  const { login, logout, token, userId, ready } = useAuth()
   const isAuthenticated = !!token
   const routes = useRoutes(isAuthenticated)
+
+  if (!ready) return <Loader />
+
   return (
     <AuthContext.Provider value={{ login, logout, token, userId, isAuthenticated }}>
       <Router>
